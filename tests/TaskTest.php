efd79483f00ae8fa92f1af5219918ca44da230e4
aundra miller and nic netzel <miller.aundra@gmail.com>
2016-02-22 11:53:21 -0800

    add find method and test

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -65,5 +65,37 @@
             $result = Task::getAll();
             $this->assertEquals([], $result);
         }
+
+        function test_getId()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $id = 1;
+            $test_Task = new Task($description, $id);
+
+            //Act
+            $result = $test_Task->getId();
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
+        function test_find()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $description2 = "Water the lawn";
+            $test_task = new Task($description);
+            $test_task->save();
+            $test_task2 = new Task($description2);
+            $test_task2->save();
+
+            //Act
+            $id = $test_task->getId();
+            $result = Task::find($id);
+
+            //Assert
+            $this->assertEquals($test_task, $result);
+        }
     }
  ?>
